ebf3d763caa00ae301e4dc16c15d95d953e7d987
David Bradway <david.bradway@gmail.com>
2016-09-11 21:46:37 -0400

    Initial commit of files to demonstrate Composition OOP in Python

---
+++ README.md
@@ -0,0 +1,4 @@
+# Python Composition
+
+This is an example of using Composition in Python to use
+methods and attributes of a given class in a new class.
---
+++ sample/core.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""Song module
+
+This module demonstrates a given class
+"""
+
+
+class Song(object):
+    """This is a Song class.
+
+    __init__ sets the lyrics attribute.
+
+    Attributes:
+        lyrics (list): a list of strings indicating lines of lyrics
+                       of the song.
+
+    """
+    def __init__(self, lyrics):
+        self.lyrics = lyrics
+
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print line
+
+    def get_character_count(self):
+        character_count = 0
+        for line in self.lyrics:
+            character_count = character_count + len(line)
+        return character_count
+
+    def make_upper(self):
+        for idx, line in enumerate(self.lyrics):
+            self.lyrics[idx] = line.upper()
