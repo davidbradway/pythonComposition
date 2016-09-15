8542c6085ac8d9ce464746e16b1510d93c9dcc34
David Bradway <dpb6@duke.edu>
2016-09-15 14:00:45 -0400

    add docstrings

--- mine.py
+++ mine.py
@@ -19,14 +19,28 @@ import sample.core
 
 
 class mySong(object):
+    """This is a mySong class.
+
+    __init__ sets the song attribute.
+
+    Attributes:
+        song (Song): a song of Class Song from the sample.core module
+
+    """
     def __init__(self, lyrics):
         self.song = sample.core.Song(lyrics)
 
     # Redefine this method, adding exclamation points to each line!
     def sing_me_a_song(self):
+        """ print song lyrics
+        """
         for line in self.song.lyrics:
             print(line+"!")
 
     # Pass this method through unchanged
     def get_character_count(self):
+        """ count characters in lyrics
+
+        :return: character count [int]
+        """
         return self.song.get_character_count()
